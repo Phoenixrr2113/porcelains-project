4bd45756eb8b7a047524dd9422dda8981d6ffd48
Randy Wilson <phoenixrr2113@gmail.com>
2019-07-03 12:14:51 -0400

    navbar routes working.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,28 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
 
 const styles = {
 	root: {
+		padding: 0,
 		flexGrow: 1,
+	},
+	toolbar: {
+		display: 'flex',
+		justifyContent: 'space-between',
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+	},
+	links: {
+		display: 'flex',
 	},
 };
 
@@ -15,15 +33,41 @@ function Navbar(props) {
 	const { classes } = props;
 
 	return (
-		<div className={classes.root}>
+		<Toolbar className={classes.root}>
 			<AppBar position='static' color='default'>
-				<Toolbar>
-					<Typography variant='h6' color='inherit'>
-						Photos
-					</Typography>
-				</Toolbar>
+				<Container maxWidth='lg'>
+					<Toolbar className={classes.toolbar}>
+						<Button className={classes.menuButton}>
+							<Link to='/'>
+								<img
+									src='https://res.cloudinary.com/dlrdfp08e/image/upload/c_scale,h_51/v1562167696/porcelains/Outlook-2p431lzt.png'
+									alt='logo'
+								/>
+							</Link>
+						</Button>
+						<div className={classes.links}>
+							<Button>
+								<Link to='/'>Main</Link>
+							</Button>
+							<Button>
+								<Link to='/about'>About</Link>
+							</Button>
+							<Button>
+								<Link to='/products'>Products</Link>
+							</Button>
+							<Button>
+								<Link to='/contact'>Contact</Link> Us
+							</Button>
+						</div>
+						<div className={classes.signin}>
+							<Button>
+								<Link to='/auth'>Sign In</Link>
+							</Button>
+						</div>
+					</Toolbar>
+				</Container>
 			</AppBar>
-		</div>
+		</Toolbar>
 	);
 }
 
